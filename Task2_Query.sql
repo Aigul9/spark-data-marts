416e5830effd2a2f39dae3f1b5a116a71a74bb5e
Aigul <aigul.sibgatulli@mail.ru>
2023-02-20 00:21:03 +0300

    push answers

---
+++ Task2_Query.sql
@@ -0,0 +1,7 @@
+SELECT sum(CrmRevenue),
+       count(*)
+FROM crm_transaction t1
+JOIN web_events t2 ON t1.UserId = t2.UserId
+AND CrmDateTime BETWEEN dateadd(mi, -5, DateTime) AND dateadd(mi, 5, DateTime)
+GROUP BY EventParam[0],
+         TrafficSource ->> 'utm_source', TrafficSource ->> 'utm_campaign', cast(CrmDateTime AS date)
